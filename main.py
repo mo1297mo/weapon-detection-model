7e127afc36ec25ad77a7e869708d38f8af4ce303
Ferdous Rahsepar <rahsepar19@gmail.com>
2025-05-26 20:55:16 +0200

    Re-add best.pt with Git LFS

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ model = YOLO("best.pt")  # Make sure best.pt is in the same folder
 
 # Track with your MacBook's webcam
 model.track(
-    source=0,                # Webcam (built-in)
-    tracker="botsort.yaml", # Built-in tracker
-    show=True,              # Show live output
-    save=True,              # Save to runs/track/predict/
-    conf=0.3                # Confidence threshold
+    source=0,  # Webcam (built-in)
+    tracker="botsort.yaml",  # Built-in tracker
+    show=True,  # Show live output
+    save=False,  # Save to runs/track/predict/
+    conf=0.3,  # Confidence threshold
 )

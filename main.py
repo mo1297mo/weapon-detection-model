d89610ddefcfc1d3c9eb7688b2f81d9ba1f7ed5e
Ferdous Rahsepar <rahsepar19@gmail.com>
2025-05-27 00:26:27 +0200

    Aktualisiere best.pt mit neuen Metadaten und verbessere die Verfolgungskonfiguration in main.py; füge zusätzliche Trainingsmetriken in results.csv hinzu

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ model = YOLO("best.pt")  # Make sure best.pt is in the same folder
 # Track with your MacBook's webcam
 model.track(
     source=0,  # Webcam (built-in)
-    tracker="botsort.yaml",  # Built-in tracker
+    tracker="botsort.yaml",  # Bult-in tracker
     show=True,  # Show live output
     save=False,  # Save to runs/track/predict/
-    conf=0.3,  # Confidence threshold
+    conf=0.7,  # Confidence threshold
 )
